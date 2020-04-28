d80ccbac3d138efc19012013ca924761a04dc446
Matt <matthave@gmail.com>
2020-04-29 01:49:21 +0200

    Create details Country section

--- src/index.js
+++ src/index.js
@@ -12,6 +12,9 @@ export default class Api {
 
     this.selectedOption = 0;
     this.darkMode = false;
+
+    this.details = document.querySelector('.details');
+    this.detailsBtn = document.querySelector('.details__btn');
   }
 
 
@@ -19,21 +22,25 @@ export default class Api {
     const main = document.querySelector('main');
     const cards = document.querySelectorAll('.card');
     const cardsBody = document.querySelectorAll('.card-body');
-    const header = document.querySelector('header');
+    const headeing = document.querySelector('.heading');
     const inputsWrap = document.querySelector('.searcher__inputWrap')
     const searcher = document.querySelector('.searcher')
     const input = document.querySelector('.form-control')
     const select = document.querySelector('.custom-select')
+    const details = document.querySelector('details');
 
     searcher.classList.toggle('searcher--darkMode');
     inputsWrap.classList.toggle('searcher__inputWrap--darkMode');
     input.classList.toggle('form-control--darkMode');
     select.classList.toggle('custom-select--darkMode')
-    header.classList.toggle('header--darkMode');
+    headeing.classList.toggle('heading--darkMode');
     main.classList.toggle('main--darkMode');
+    details.classList.toggle('details--darkMode')
+
     cards.forEach((card) => {
       card.classList.toggle('card--darkMode')
     })
+
     cardsBody.forEach((card) => {
       card.classList.toggle('card-body--darkMode')
     })
@@ -43,6 +50,10 @@ export default class Api {
 
   start() {
     this.darkModeTheme.addEventListener('click', () => this.darkModeChange())
+    this.detailsBtn.addEventListener('click', () => {
+      this.details.style.left = '-110%';
+      document.body.style.overflow = 'inherit';
+    })
     this.select.addEventListener('change', (e) => {
       this.selectedOption = e.target.options.selectedIndex;
       this.fetchAPI()
@@ -116,7 +127,9 @@ export default class Api {
       const allCountriesArr = [...this.allCountries];
       allCountriesArr.forEach((country) => {
         const card = document.createElement('div')
-        card.setAttribute('class', `card align-items-center ${this.darkMode ? 'card--darkMode' : ''}`)
+        card.setAttribute('class', `card align-items
+        -center ${this.darkMode ? 'card--darkMode' : ''}`)
+        card.addEventListener('click', () => this.detailsCard(country.name))
 
         const cardImg = document.createElement('div');
         cardImg.setAttribute('class', 'card__img')
@@ -155,6 +168,7 @@ export default class Api {
       allCountriesArr.forEach((country) => {
         const card = document.createElement('div')
         card.setAttribute('class', `card align-items-center ${this.darkMode ? 'card--darkMode' : ''}`)
+        card.addEventListener('click', () => this.detailsCard(country.name))
 
         const cardImg = document.createElement('div');
         cardImg.setAttribute('class', 'card__img')
@@ -188,6 +202,43 @@ export default class Api {
         this.container.appendChild(card)
       })
     }
+  }
+
+  detailsCard(name) {
+    const nameOfCountry = document.querySelector('.name');
+    const native = document.querySelector('.native');
+    const population = document.querySelector('.population');
+    const region = document.querySelector('.countryRegion');
+    const sub = document.querySelector('.sub');
+    const capital = document.querySelector('.capital');
+    const topLevel = document.querySelector('.topLevel');
+    const currencies = document.querySelector('.currencies');
+    const lang = document.querySelector('.lang');
+    const flag = document.querySelector('.details__img');
+    const borderOne = document.querySelector('.borderOne');
+    const borderTwo = document.querySelector('.borderTwo');
+    const borderThree = document.querySelector('.borderThree');
+
+    const detailsOfCountry = this.allCountries.filter((country) => {
+      return country.name === name
+    })
+
+    flag.style.backgroundImage = `url(${detailsOfCountry[0].flag})`
+    nameOfCountry.textContent = `${detailsOfCountry[0].name}`;
+    native.textContent = `${detailsOfCountry[0].nativeName}`;
+    population.textContent = `${detailsOfCountry[0].population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`;
+    region.textContent = `${detailsOfCountry[0].region}`;
+    sub.textContent = `${detailsOfCountry[0].subregion}`;
+    capital.textContent = `${detailsOfCountry[0].capital}`;
+    topLevel.textContent = `${detailsOfCountry[0].topLevelDomain}`;
+    currencies.textContent = `${detailsOfCountry[0].currencies[0].name}`;
+    lang.textContent = `${detailsOfCountry[0].languages[0].name}`;
+    borderOne.textContent = `${detailsOfCountry[0].borders[0]}`;
+    borderTwo.textContent = `${detailsOfCountry[0].borders[1]}`;
+    borderThree.textContent = `${detailsOfCountry[0].borders[2]}`;
+
+    document.body.style.overflow = 'hidden'
+    this.details.style.left = '0'
   }
 
 }
